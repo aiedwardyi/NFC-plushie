453b839b0a081a83e898c1440ce03eedc402b46f
aiedwardyi <41576951+aiedwardyi@users.noreply.github.com>
2026-09-10 15:35:57 +0900

    fix: restore broad XSS onerror coverage in name escape test

--- tests/app.test.js
+++ tests/app.test.js
@@ -177,7 +177,7 @@ test("name boundaries and HTML escaping", async (t) => {
   }
   answers.tap = "OWNER";
   const res = await request(tapUrl());
-  assert.doesNotMatch(res.html, /<img src=x onerror=x>/);
+  assert.doesNotMatch(res.html, /<img[^>]*onerror/i);
   assert.match(res.html, /&lt;img src=x onerror=x&gt;/);
 });
 
